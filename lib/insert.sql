d98a8e56513286cc21ce8f85d39447db70c55169
MJ Johnson <mjohnson4685@gmail.com>
2020-10-12 23:43:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, "Scottish Fold", 1000000);
