d147a068040be782d9c047a29d02816a90306568
MJ Johnson <mjohnson4685@gmail.com>
2020-10-12 23:53:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 9, "F", "black", "nice", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "M", "brown", "mean", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 1, "F", "black", "mean", 0));
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 90, "M", "brown", "mean", 0));
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8,NULL, 90, "M", "brown", "mean", 0));
