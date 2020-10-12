913b5d52a72795a6d6b81943ecaebcdfc89a8b00
MJ Johnson <mjohnson4685@gmail.com>
2020-10-12 23:54:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 10, "M", "brown", "nice", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 12, "M", "brown", "nice", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 3, "F", "black", "nice", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 5, "M", "brown", "nice", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 9, "F", "black", "nice", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "M", "brown", "mean", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 1, "F", "black", "mean", 0));
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8,NULL, 90, "M", "brown", "mean", 0));
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 10, "M", "brown", "nice", 1),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 12, "M", "brown", "nice", 0),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 3, "F", "black", "nice", 1),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 5, "M", "brown", "nice", 0),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 9, "F", "black", "nice", 0),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "M", "brown", "mean", 1),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 1, "F", "black", "mean", 0),
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8,NULL, 90, "M", "brown", "mean", 0),
